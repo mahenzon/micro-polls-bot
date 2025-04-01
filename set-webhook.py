14e18aeb39b3c2735b7eacc4e85449be01f7282b
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-01 19:07:55 +0300

    create util to set webhook

---
+++ set-webhook.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+
+from bot import create_bot
+from core import settings
+
+log = logging.getLogger(__name__)
+
+
+async def main() -> None:
+    bot = create_bot()
+    log.warning("Delete webhook")
+    await bot.delete_webhook()
+    log.warning("Set webhook")
+    await bot.set_webhook(
+        url=settings.api_gateway_url,
+    )
+    log.warning("Done!")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
