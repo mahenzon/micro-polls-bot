131a56a463cc247d82e8932031e13c2f09672e8d
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-02 19:56:10 +0300

    provide allowed updates on set webhook

--- set-webhook.py
+++ set-webhook.py
@@ -11,13 +11,16 @@ async def main() -> None:
     bot = create_bot()
     log.warning("Me %s", await bot.get_me())
     log.warning("Delete webhook")
-    await bot.delete_webhook()
+    await bot.delete_webhook(drop_pending_updates=True)
     log.warning("Set webhook")
     await bot.set_webhook(
         url=settings.api_gateway_url,
+        allowed_updates=[],
     )
     log.warning("Done!")
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+# todo: typer
