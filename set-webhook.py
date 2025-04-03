9836d5ff322dde20335b080a03bba4a1e4e79017
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-03 14:00:11 +0300

    handle only allowed updates

--- set-webhook.py
+++ set-webhook.py
@@ -10,12 +10,14 @@ log = logging.getLogger(__name__)
 async def main() -> None:
     bot = create_bot()
     log.warning("Me %s", await bot.get_me())
+    print(await bot.get_webhook_info())
     log.warning("Delete webhook")
     await bot.delete_webhook(drop_pending_updates=True)
     log.warning("Set webhook")
     await bot.set_webhook(
         url=settings.api_gateway_url,
-        allowed_updates=[],
+        # skip inline for now
+        allowed_updates=["message", "callback_query"],
     )
     log.warning("Done!")
 
