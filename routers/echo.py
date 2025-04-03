f5587bbff257d8f0f92773075abd897d6b0929fc
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-03 13:58:55 +0300

    send echo into response

--- routers/echo.py
+++ routers/echo.py
@@ -1,5 +1,7 @@
 __all__ = ("router",)
 
+from typing import Any
+
 from aiogram import F, Router
 from aiogram.types import Message, ReactionTypeEmoji
 
@@ -7,8 +9,7 @@ router = Router(name="echo")
 
 
 @router.message(F.text)
-async def message_handler(message: Message) -> None:
+async def message_handler(message: Message) -> Any:
     if message.chat.type == "private":
-        await message.answer(message.text)
-        return
-    await message.react([ReactionTypeEmoji(emoji="👍")])
+        return message.answer(message.text)
+    return message.react([ReactionTypeEmoji(emoji="👍")])
